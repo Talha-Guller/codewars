a385ecd4bee38fee909b363a1045051021e1057d
Talha GÜLLER <83316373+Talha-Guller@users.noreply.github.com>
2021-10-10 13:25:06 +0300

    Create Even-or-Odd.java

---
+++ Even-or-Odd.java
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0){
+                    return "Even";
+                }
+        return "Odd"; 
+        
+    }
+}

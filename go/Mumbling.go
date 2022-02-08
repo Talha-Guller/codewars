3991a1483b20e036a670ca9425b31bdc9bd2e498
Talha Guller <talhaguller7@gmail.com>
2022-02-08 16:49:48 +0300

    go

---
+++ go/Mumbling.go
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/go
+package kata
+
+import (
+	"strings"
+)
+
+func Accum(s string) string {
+
+	str := make([]string, len(s))
+
+	for i := 0; i < len(s); i++ {
+		str[i] = strings.ToUpper(string(s[i])) + strings.ToLower(strings.Repeat(string(s[i]), i))
+	}
+
+	return strings.Join(str, "-")
+}
